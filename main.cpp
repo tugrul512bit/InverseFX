fe95c1ad4c252b4bdde56f3f047a18b09f489e92
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2022-07-13 21:53:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int main()
 		// f(x)=x*x as if we are unable to compute sqrt(x) and we don't even know what f(x) is doing
 		return inp*inp;
 	});
-	std::cout<<inv.computeInverseLowQuality(3.14f,0.00001f)<<std::endl;
+	std::cout<<inv.computeInverseLowQuality(3.1415f,0.00001f)<<std::endl;
 	return 0;
 }
 
-// outputs 1.77
+// outputs 1.77243
