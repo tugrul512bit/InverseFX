006012363e424d998e9b0f2a4af5a6577c81334f
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2022-07-13 22:00:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 int main()
 {
 	InverseFX::ScalarInverse<float,float> inv([](float inp){
-		// black-box function sample
-		// f(x)=x*x as if we are unable to compute sqrt(x) and we don't even know what f(x) is doing
+		// black-box function sample 
+		// as if we are unable to compute sqrt(x) and don't know what f(x) is doing
+		// f(x)=x*x
 		return inp*inp;
 	});
 	std::cout<<inv.computeInverseLowQuality(3.1415f,0.00001f)<<std::endl;
