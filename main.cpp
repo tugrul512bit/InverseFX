6ba442475655a6f11541a72021934fef978ad108
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2022-07-13 22:14:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
-#include <iostream>
+#include<iostream>
 
 #include"InverseFX.h"
 int main()
 {
 	InverseFX::ScalarInverse<float,float> inv([](float inp){
-		// black-box function sample 
-		// as if we are unable to compute sqrt(x) and don't know what f(x) is doing
+		// black-box function sample
 		// f(x)=x*x
 		return inp*inp;
-	});
-	std::cout<<inv.computeInverseLowQuality(3.1415f,0.00001f)<<std::endl;
+	},0.00001f);
+	std::cout<<inv.computeInverseLowQuality(3.1415f)<<std::endl;
 	return 0;
 }
 
-// outputs 1.77243
+// outputs 1.77243 by calling f(x) 3 times, computing floating-point multiplication 1 time, division 1 time
