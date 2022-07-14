381be6133d653c7eec792be9b42eb781961c5522
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2022-07-14 21:15:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -115,85 +115,82 @@ int main()
 
 
 /*
-1.77243 -----> square root (inverse of x*x) of 3.1415
+1.77243 --> inverse of x*x (sqrt(x)) of 3.1415 
 
-scalar work on array:
-0.00248134 elements per nanosecond
-0.00234355 elements per nanosecond
-0.00246709 elements per nanosecond
-0.00249108 elements per nanosecond
-0.00247686 elements per nanosecond
-0.00249032 elements per nanosecond
-0.00249328 elements per nanosecond
-0.00247671 elements per nanosecond
-0.00248345 elements per nanosecond
-0.00248592 elements per nanosecond
-0.0024748 elements per nanosecond
-0.00248991 elements per nanosecond
-0.00249236 elements per nanosecond
-0.00249008 elements per nanosecond
-0.00237878 elements per nanosecond
-0.00247678 elements per nanosecond
-0.00249626 elements per nanosecond
-0.00248505 elements per nanosecond
-0.00248557 elements per nanosecond
-0.00249246 elements per nanosecond
+scalar:
+0.00232384 elements per nanosecond
+0.00246791 elements per nanosecond
+0.00244565 elements per nanosecond
+0.00244049 elements per nanosecond
+0.00223346 elements per nanosecond
+0.00228206 elements per nanosecond
+0.00249925 elements per nanosecond
+0.00248752 elements per nanosecond
+0.00243588 elements per nanosecond
+0.00247882 elements per nanosecond
+0.00243925 elements per nanosecond
+0.00225988 elements per nanosecond
+0.00242437 elements per nanosecond
+0.00204892 elements per nanosecond
+0.00240362 elements per nanosecond
+0.00247664 elements per nanosecond
+0.00225022 elements per nanosecond
+0.00248844 elements per nanosecond
+0.00244302 elements per nanosecond
+0.00232976 elements per nanosecond
 1.22481e+06
 
-parallel work on array with only scalar f(x) given by user:
-0.00516736 elements per nanosecond
-0.00519919 elements per nanosecond
-0.00521338 elements per nanosecond
-0.0052303 elements per nanosecond
-0.00524147 elements per nanosecond
-0.00520205 elements per nanosecond
-0.00521525 elements per nanosecond
-0.00524055 elements per nanosecond
-0.00520077 elements per nanosecond
-0.00523028 elements per nanosecond
-0.00517554 elements per nanosecond
-0.00524118 elements per nanosecond
-0.00521376 elements per nanosecond
-0.00520089 elements per nanosecond
-0.00524106 elements per nanosecond
-0.00519718 elements per nanosecond
-0.00519525 elements per nanosecond
-0.00521436 elements per nanosecond
-0.00524124 elements per nanosecond
-0.00517988 elements per nanosecond
+parallelized newton-raphson using scalar f(x): ~2x speedup
+0.00494962 elements per nanosecond
+0.00501129 elements per nanosecond
+0.00434865 elements per nanosecond
+0.00502371 elements per nanosecond
+0.00500942 elements per nanosecond
+0.00500945 elements per nanosecond
+0.00504377 elements per nanosecond
+0.00501537 elements per nanosecond
+0.00485419 elements per nanosecond
+0.00448336 elements per nanosecond
+0.00502426 elements per nanosecond
+0.00504887 elements per nanosecond
+0.0049874 elements per nanosecond
+0.00439351 elements per nanosecond
+0.004798 elements per nanosecond
+0.00504869 elements per nanosecond
+0.00499643 elements per nanosecond
+0.00502452 elements per nanosecond
+0.00482616 elements per nanosecond
+0.00438719 elements per nanosecond
 1.22481e+06
 
-parallel work on array with parallel f(x) given by user:
-0.0114558 elements per nanosecond
-0.0110267 elements per nanosecond
-0.0112143 elements per nanosecond
-0.0112153 elements per nanosecond
-0.0111488 elements per nanosecond
-0.0109558 elements per nanosecond
-0.0115662 elements per nanosecond
-0.0113101 elements per nanosecond
-0.0115026 elements per nanosecond
-0.0115608 elements per nanosecond
-0.0113773 elements per nanosecond
-0.0115689 elements per nanosecond
-0.0115629 elements per nanosecond
-0.011143 elements per nanosecond
-0.0112179 elements per nanosecond
-0.0112201 elements per nanosecond
-0.01098 elements per nanosecond
-0.0112175 elements per nanosecond
-0.0112157 elements per nanosecond
-0.0110193 elements per nanosecond
+parallelized newton-raphson + parallel user-f(x)
+0.0208567 elements per nanosecond
+0.0223288 elements per nanosecond
+0.0223335 elements per nanosecond
+0.0223346 elements per nanosecond
+0.0223441 elements per nanosecond
+0.021363 elements per nanosecond
+0.0223494 elements per nanosecond
+0.0223393 elements per nanosecond
+0.0223432 elements per nanosecond
+0.0223512 elements per nanosecond
+0.0219458 elements per nanosecond
+0.0223504 elements per nanosecond
+0.0223497 elements per nanosecond
+0.0223492 elements per nanosecond
+0.0223525 elements per nanosecond
+0.0223508 elements per nanosecond
+0.0215053 elements per nanosecond
+0.0136961 elements per nanosecond
+0.0221349 elements per nanosecond
+0.0223504 elements per nanosecond
 1.22481e+06
 
-~5.5x speedup with FX8150's AVX
+~9x speedup with FX8150's AVX
 
 */
 
-// With godbolt.org's server that has AVX2 (and relevant compiler flags enabled)
-// 0.00575837 elements per nanosecond ---> scalar
+// With godbolt.org's server that has AVX512 (and relevant compiler flags enabled)
+// 0.00665837 elements per nanosecond ---> scalar
 // 0.0121961 elements per nanosecond ----> parallel run with only scalar f(x) given for constructor
-// 0.0341529 elements per nanosecond ----> fully parallel (~6x speedup)
-
-// With AVX512 CPU of godbolt.org server (and avx512f flag enabled with preferred vector width=512)
-// 0.0403193 elements per nanosecond ----> fully parallel with lower frequency than AVX2 (~7x speedup depending on server-load)
+// 0.110637 elements per nanosecond ----> fully parallel (~17x speedup)
